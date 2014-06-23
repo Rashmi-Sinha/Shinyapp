e225bc924c0a13992545143f1956e6df171b006f
rashmiBhushan <rashmiBhushan@users.noreply.github.com>
2014-06-23 00:35:28 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,56 +1,57 @@
 library(shiny)
-library(caret)
-library(randomForest)
+library(datasets)
 
-irisClass<- function (SL,SW,PL,PW) {
-        # Create data directory
-        if(!file.exists("./data")){dir.create("./data")}
-        
-        # Download test file
-        url <- ("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
-        download.file(url, "./data/iris.csv")
-        
-        # Change to data directory for reading the data
-        setwd("./data/")
-        
-        # Load datasets
-        iris <- read.csv("iris.csv", header=FALSE, sep=",")
-        
-        #Nombre de las variables
-        colnames(iris)<-c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
-
-        set.seed(1234)
-        ind<-sample(2,nrow(iris),replace=TRUE,prob=c(0.7,0.3))
-        irisTrain<-iris[ind==1,]
-        irisTest<-iris[ind==2,]
-        colnames(irisTrain)<-c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
-        colnames(irisTest)<-c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
-        
-        rf_model <- randomForest(Species ~ ., irisTrain, ntree=500)
-        
-        training_pred <- predict(rf_model, irisTrain)
-        testing_pred <- predict(rf_model, irisTest)
-        
-        Sepal.Length<-SL
-        Sepal.Width<-SW
-        Petal.Length<-PL
-        Petal.Width<-PW
-        
-        prtesting<-matrix(c(Sepal.Length,Sepal.Width,Petal.Length,Petal.Width),nrow=1,ncol=4)
-        answer <- predict(rf_model, prtesting)
-        answer
-}
-
-
-# Change directory
-setwd("./")
-
-shinyServer(
-        function(input, output) {
-                output$SL<-renderPrint({input$SL})
-                output$SW<-renderPrint({input$SW})
-                output$PL<-renderPrint({input$PL})
-                output$PW<-renderPrint({input$PW})
-                output$prediction<-renderPrint({irisClass(input$SL,input$SW,input$PL,input$PW)})
-        }
-)
+# Define server logic required to summarize and view the selected dataset
+shinyServer(function(input, output) {
+  
+  # By declaring databaseInput as a reactive expression we ensure that:
+  #
+  #  1) It is only called when the inputs it depends on changes
+  #  2) The computation and result are shared by all the callers (it 
+  #     only executes a single time)
+  #
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "faithful" = faithful,
+           "trees" = trees,
+           "iris" = iris)
+  })
+  
+  # The output$caption is computed based on a reactive expression that
+  # returns input$caption. When the user changes the "caption" field:
+  #
+  #  1) This function is automatically called to recompute the output 
+  #  2) The new caption is pushed back to the browser for re-display
+  # 
+  # Note that because the data-oriented reactive expressions below don't 
+  # depend on input$caption, those expressions are NOT called when 
+  # input$caption changes.
+  output$caption <- renderText({
+    input$caption
+  })
+  
+  # The output$summary depends on the datasetInput reactive expression, 
+  # so will be re-executed whenever datasetInput is invalidated
+  # (i.e. whenever the input$dataset changes)
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  
+  # The output$pairplot depends on the datasetInput reactive expression, 
+  # so will be re-executed whenever datasetInput is invalidated
+  # (i.e. whenever the input$dataset changes)
+  output$pairplot <- renderPlot({
+    dataset <- datasetInput()
+    pairs(dataset)
+  })
+  
+  
+  # The output$view depends on both the databaseInput reactive expression
+  # and input$obs, so will be re-executed whenever input$dataset or 
+  # input$obs is changed. 
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+})
